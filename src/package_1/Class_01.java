772040d983679fec6cce8659a01e576b4f32e89c
Karan Singh <jpannu10@gmail.com>
2022-03-08 16:06:13 -0500

    First commit

---
+++ src/package_1/Class_01.java
@@ -0,0 +1,11 @@
+package package_1;
+
+public class Class_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Package 1 > Class 1");
+	}
+
+}
---
+++ src/package_1/Class_02.java
@@ -0,0 +1,10 @@
+package package_1;
+
+public class Class_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Package 1 > Class 1");
+	}
+
+}
